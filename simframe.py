453862645259c4789e41b1f0b5be99939a27b260
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-06 17:12:15 -0400

    time to actually do physics

--- simframe.py
+++ simframe.py
@@ -1,10 +1,25 @@
 from PIL import Image
 import numpy as np
 
+DEFAULT_CURRENT = 1  # Amps
+
 class SimFrame:
-    def __init__(self, path):
+    def __init__(self, path, current=DEFAULT_CURRENT):
         pic = Image.open(path, "r")
         self.arr = np.asarray(pic)[:,:,0] > 200
+        self.nocurrent = self.arr.sum() == 0
+        if self.nocurrent:
+            self.current_density = None
+            return
+        cross_sectional_area = self.arr.sum() / self.arr.size
+        self.current_density = current / cross_sectional_area
 
-print(SimFrame("frames/BadApple_358.jpg").arr)
+    def __str__(self):
+        cd = f"J = {self.current_density}"
+        return f"< SimFrame ({self.arr.size} pixels) with {cd} >"
+
+test = SimFrame("frames/BadApple_358.jpg")
+
+if __name__ == "__main__":
+    print(test)
 
