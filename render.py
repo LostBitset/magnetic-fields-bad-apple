316116177b5452784bf28b242a2b7a5429ec045b
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-06 19:40:03 -0400

    first one, need to make gitignore but will leave this one in

---
+++ render.py
@@ -0,0 +1,21 @@
+from simframe import SimFrame
+import cv2
+
+# Frames range from 0 to 6571
+
+def process_frame(n):
+    pfx = f"[process_frame] (Frame {n}): "
+    print(pfx + "Loading frame...")
+    sim = SimFrame(f"frames/BadApple_{n}.jpg")
+    print(pfx + "Calculating B-field...")
+    sim.bake_b_field()
+    print(pfx + "Drawing B-field...")
+    image = sim.draw_b_field()
+    print(pfx + "Saving new image...")
+    status = cv2.imwrite(f"outframes/BadAppleBField_{n}.jpg", image)
+    if not status:
+        raise Exception("ERROR!! Failed to save image.")
+    print(pfx + "All done.")
+
+process_frame(810)
+
