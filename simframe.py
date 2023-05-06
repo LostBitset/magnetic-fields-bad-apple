a0a975be38e7714218c1e1cb2be694b6f0181cec
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-06 17:49:08 -0400

    better but still need to make the b-field lower res

--- simframe.py
+++ simframe.py
@@ -1,12 +1,19 @@
 from PIL import Image
 import numpy as np
+from skimage.measure import block_reduce
+import math
 
 DEFAULT_CURRENT = 1  # Amps
+
+# Magnetic permeability of free space
+MU_0 = 1.25663706212e-6
 
 class SimFrame:
     def __init__(self, path, current=DEFAULT_CURRENT):
         pic = Image.open(path, "r")
-        self.arr = np.asarray(pic)[:,:,0] > 200
+        self.arr = np.asarray(pic)[:,:,0]
+        self.arr = block_reduce(self.arr, block_size=(5, 5), func=np.mean)
+        self.arr = self.arr > 200
         self.nocurrent = self.arr.sum() == 0
         if self.nocurrent:
             self.current_density = None
@@ -20,11 +27,34 @@ class SimFrame:
 
     def b_field(self):
         field = np.zeros((*self.arr.shape, 2))
-        # TODO the actual thing lol
+        for cx, row in enumerate(self.arr):
+            for cy, is_conductor in enumerate(row):
+                if is_conductor:
+                    print("Calculating contributions of conducting volume element...")
+                    for px in range(self.arr.shape[0]):
+                        for py in range(self.arr.shape[1]):
+                            jdv = np.array([
+                                0.,
+                                0.,
+                                self.current_density,
+                            ])
+                            r = np.array([
+                                cx - px,
+                                cy - py,
+                                0.,
+                            ])
+                            cross_product = np.cross(jdv, r)
+                            dist = np.linalg.norm(r)
+                            contribution = cross_product
+                            contribution /= dist ** 3
+                            contribution *= MU_0
+                            contribution /= 4 * math.pi
+                            field[px, py] += contribution[0:2]
         return field
 
 test = SimFrame("frames/BadApple_358.jpg")
 
 if __name__ == "__main__":
     print(test)
+    print(test.arr)
 
